72d7340a3d96b320c744ad01047d0272a2a5732c
ShanukaAlahakoon <shanukaalahakoon456@gmail.com>
2025-11-23 15:21:38 +0530

    Add Google login functionality and user retrieval endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,7 @@ import User from "../models/User.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import axios from "axios";
 
 dotenv.config();
 
@@ -16,8 +17,6 @@ export function createUser(req, res) {
     password: hashedPassword,
     firstName: data.firstName,
     lastName: data.lastName,
-    role: data.role,
-    image: data.image,
   });
 
   user.save().then(() => {
@@ -73,4 +72,75 @@ export function isAdmin(req) {
   }
 
   return true;
+}
+
+export function getUser(req, res) {
+  if (req.user == null) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  res.json(req.user);
+}
+
+export async function googleLogin(req, res) {
+  console.log(req.body.token);
+  try {
+    const response = await axios.get(
+      "https://www.googleapis.com/oauth2/v3/userinfo",
+      {
+        headers: {
+          Authorization: `Bearer ${req.body.token}`,
+        },
+      }
+    );
+    console.log(response.data);
+    const user = await User.findOne({ email: response.data.email });
+    if (user == null) {
+      const newUser = new User({
+        email: response.data.email,
+        firstName: response.data.given_name,
+        lastName: response.data.family_name,
+        image: response.data.picture,
+        password: "123",
+      });
+      await newUser.save();
+      const payload = {
+        email: newUser.email,
+        firstName: newUser.firstName,
+        lastName: newUser.lastName,
+        role: newUser.role,
+        image: newUser.image,
+        isEmailVerified: newUser.isEmailVerified,
+      };
+      const token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: "150h",
+      });
+      res.json({
+        message: "Login successful",
+        token: token,
+        role: newUser.role,
+      });
+    } else {
+      const payload = {
+        email: user.email,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        role: user.role,
+        image: user.image,
+        isEmailVerified: user.isEmailVerified,
+      };
+      const token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: "150h",
+      });
+
+      res.json({
+        message: "Login successful",
+        token: token,
+        role: user.role,
+      });
+    }
+  } catch (error) {
+    console.error("Google login error:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
 }
