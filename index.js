7c9d0f23e636715fa74ab65d278a16a48aef4948
ShanukaAlahakoon <shanukaalahakoon456@gmail.com>
2025-12-19 13:07:42 +0530

    Implement review functionality with add, fetch, delete, and list endpoints

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import mongoose, { mongo, Mongoose } from "mongoose";
 import userRouter from "./routes/userRouter.js";
 import productRouter from "./routes/productRouter.js";
 import orderRouter from "./routes/orderRouter.js";
+import reviewRouter from "./routes/reviewRouter.js";
 import jwt from "jsonwebtoken";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -45,6 +46,7 @@ app.use((req, res, next) => {
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
 app.use("/api/orders", orderRouter);
+app.use("/api/reviews", reviewRouter);
 app.listen(4000, () => {
   console.log("Server is running on port 4000");
 });
